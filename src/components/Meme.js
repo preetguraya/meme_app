6921683335aea264923df87c2752bc7fd1579a12
Gurpreet Kaur <gkguraya5@gmail.com>
2022-10-12 21:26:16 -0700

    image alt

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -56,7 +56,7 @@ export default function Meme() {
                 </button>
             </div>
             <div className="meme">
-                <img src={meme.randomImage} className="meme--image" />
+                <img src={meme.randomImage} className="meme--image" alt="img"/>
                 <h2 className="meme--text top">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
